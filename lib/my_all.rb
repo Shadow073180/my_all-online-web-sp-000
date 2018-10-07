6ce13f5a9271fab202c2aaeb89160f9465dff126
David masterson <david73180@gmail.com>
2018-10-07 02:07:09 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,11 +4,7 @@ def my_all?(collection)
   i = 0
   collections = []
   while i < collection.length
-    collections << yield(collection[i])
-  i += 1
-end
-end
-
-def include?(collections)
- collections.include?(false)
+    yield collection[i]
+    i += 1
+  end
 end
