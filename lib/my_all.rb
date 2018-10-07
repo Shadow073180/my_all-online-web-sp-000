d72f8369e50d7ff7c5711905a325d1e3649e3cfa
David masterson <david73180@gmail.com>
2018-10-07 02:13:16 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   collections = []
   while i < collection.length
-    block_return_values << yield(collection[i])
+    collections << yield(collection[i])
     i = i + 1
   end
 
